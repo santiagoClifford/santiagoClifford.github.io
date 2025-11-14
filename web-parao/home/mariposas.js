0d00b2a780a0ba768bd5556c977fe45567fc42f2
clifford1one <santiago.gaete@mail.udp.cl>
2025-11-14 17:59:25 -0300

    reduccion js

--- web-parao/home/mariposas.js
+++ web-parao/home/mariposas.js
@@ -1,8 +1,11 @@
-let frame1;
-let frame2;
+let frame1, frame2;
 let mariposas = [];
-let mariposaQty=4;
-
+const CONFIG = {
+  qty: 4,
+  labels: ['proyectos', 'about me', 'sketches', 'contacto'],
+  links: ['https://ejemplo1.com', 'https://ejemplo2.com', 'https://ejemplo3.com', 'https://ejemplo4.com'],
+  colors: ['#FF6B6B', '#4ECDC4', '#FFD166', '#845EC2']
+};
 
 function preload() {
   frame1 = loadImage('../imgs/mariposa1.png');
@@ -10,150 +13,85 @@ function preload() {
 }
 
 function setup() {
-  // create canvas for butterflies and give it an id so CSS can layer it
-  let c = createCanvas(windowWidth, windowHeight*2);
-  c.position(0, 0);
-  c.style('position', 'fixed');
-  c.style('top', '0px');
-  c.style('left', '0px');
-  c.id('mariposasCanvas');
-  // allow pointer events so we can click butterflies
-  c.style('pointer-events', 'auto');
-  //background(0,0);
-
-  imageMode(CENTER);  
- //  tiempoTranscurrido=millis();
+  createCanvas(windowWidth, windowHeight);
+  imageMode(CENTER);
   
- //10 mariposas 
- for (let i = 0; i < mariposaQty; i++) {
-   let x = random(width);
-   let y = random(height);
-   let w = random(40, 50);
-   let h = w;
-   let labels = ['proyectos', 'about me', 'sketches','contacto'];
-   let links = [
-     'https://ejemplo1.com',
-     'https://ejemplo2.com',
-     'https://ejemplo3.com',
-     'https://ejemplo4.com',
-   ];
-   // Colores fijos (una paleta). Cada mariposa toma uno.
-   let colors = ['#FF6B6B', '#4ECDC4', '#FFD166', '#845EC2'];
-   let label = labels[i % labels.length];
-   let link = links[i % links.length];
-   let rectColor = colors[i % colors.length];
-   //mariposas.push añade un elemento al array mariposas[] (ahora con color fijo)
-   mariposas.push(new Marisopa(x, y, w, h, frame1, frame2, label, link, rectColor));
- }
+  for (let i = 0; i < CONFIG.qty; i++) {
+    mariposas.push(new Marisopa(
+      random(width), random(height), random(40, 50),
+      frame1, frame2,
+      CONFIG.labels[i % CONFIG.labels.length],
+      CONFIG.links[i % CONFIG.links.length],
+      CONFIG.colors[i % CONFIG.colors.length]
+    ));
+  }
 }
 
 function draw() {
-  //background(220);
-  //background(100, 100, 240, 10);
   clear();
-
-  //mostrar y mover todas las mariposas
-  for (let i = 0; i < mariposaQty; i++) {
-    //para cada mariposa del array, aplicar el método move y show
-    mariposas[i].move();
-    mariposas[i].show();
-  }
+  mariposas.forEach(m => { m.move(); m.show(); });
 }
 
 function mousePressed() {
   for (let m of mariposas) {
     if (m.isClicked(mouseX, mouseY)) {
       m.onClick();
-      break; // optional, stops at first one
+      break;
     }
   }
 }
 class Marisopa {
-  constructor(x, y, w, h, img1, img2, label, link, rectColor) {
+  constructor(x, y, w, img1, img2, label, link, rectColor) {
     this.x = x;
     this.y = y;
     this.w = w;
-    this.h = h;
     this.img1 = img1;
     this.img2 = img2;
     this.label = label;
     this.link = link;
-    // Guardar color fijo para el rectángulo (si se pasa como string hexadecimal)
-    this.rectColor = rectColor ? color(rectColor) : color(255, 100, 150);
-    this.lastX = this.x; // en el constructor
-
-    
-        // Semillas únicas para el movimiento
+    this.color = color(rectColor);
+    this.lastX = x;
     this.tx = random(1000);
     this.ty = random(1000);
   }
 
-show() {
-  let frame = int((millis() / 160) % 2);
-  
-  // Ver si va hacia la derecha o izquierda
-  let mirandoDerecha = this.x - this.lastX > 0;
+  show() {
+    let frame = int((millis() / 160) % 2);
+    let mirandoDerecha = this.x - this.lastX > 0;
 
-  push();
-  translate(this.x, this.y);
+    push();
+    translate(this.x, this.y);
+    if (!mirandoDerecha) scale(-1, 1);
+    image(frame === 0 ? this.img1 : this.img2, 0, 0, this.w, this.w);
+    pop();
 
-  if (!mirandoDerecha) {
-    scale(-1, 1); // voltear horizontalmente
+    push();
+    fill(this.color);
+    stroke(0);
+    strokeWeight(2);
+    rect(this.x - 30, this.y - this.w / 2 - 40, 60, 25);
+    fill(0);
+    textAlign(CENTER, CENTER);
+    textSize(12);
+    noStroke();
+    text(this.label, this.x, this.y - this.w / 2 - 27);
+    pop();
   }
 
-  // Dibujar centrado en su posición
-  let img = frame === 0 ? this.img1 : this.img2;
-  image(img, 0, 0, this.w, this.h);
+  move() {
+    this.lastX = this.x;
+    this.x = map(noise(this.tx), 0, 1, 0, width);
+    this.y = map(noise(this.ty), 0, 1, 0, height);
+    this.tx += 0.0021;
+    this.ty += 0.002;
+  }
 
-  pop();
-
-  // Dibujar rectángulo y texto arriba de la mariposa
-  push();
-  // Usar el color fijo asignado a esta mariposa
-  fill(this.rectColor);
-  stroke(0);
-  strokeWeight(2);
-  let rectWidth = 60;
-  let rectHeight = 25;
-  rect(this.x - rectWidth / 2, this.y - this.h / 2 - 40, rectWidth, rectHeight);
-
-  // Dibujar texto
-  fill(0); // Color del texto (negro)
-  textAlign(CENTER, CENTER);
-  textSize(12);
-  noStroke();
-  text(this.label, this.x, this.y - this.h / 2 - 27);
-  pop();
-}
-move() {
-  // Guardar posición anterior ANTES de mover
-  this.lastX = this.x;
-
-  // Movimiento con ruido Perlin
-  this.x = map(noise(this.tx), 0, 1, 0, width);
-  this.y = map(noise(this.ty), 0, 1, 0, height);
-
-  // Avanzar el ruido
-  this.tx += 0.0021;
-  this.ty += 0.002;
-}
-
-     //<https://editor.p5js.org/ss14736/sketches/xMw_lY0s7>
-
-        isClicked(px, py) {
-    // simple hitbox (rectangular)
-    return (
-      px > this.x - this.w / 2 &&
-      px < this.x + this.w / 2 &&
-      py > this.y - this.h / 2 &&
-      py < this.y + this.h / 2
-    );
+  isClicked(px, py) {
+    return px > this.x - this.w / 2 && px < this.x + this.w / 2 &&
+           py > this.y - this.w / 2 && py < this.y + this.w / 2;
   }
 
   onClick() {
-    // what happens when clicked
-    console.log("Butterfly clicked! Link: " + this.link);
     window.open(this.link, "_blank");
   }
-
 }
